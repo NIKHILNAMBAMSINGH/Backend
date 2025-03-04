0618655129fc65b08fcf213db87227d736becb78
nikhil <nsingh@cdac.in>
2025-03-05 00:41:08 +0530

    changes made

--- ProfileMs/src/main/java/com/Backend/ProfileMs/DTO/PatientDTO.java
+++ ProfileMs/src/main/java/com/Backend/ProfileMs/DTO/PatientDTO.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PatientDTO {
+    private Long id;
     private String name;
     private String email;
     private LocalDate dob;
