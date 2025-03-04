fe81547eef1b273342595af28c97da14b4df38c7
nikhil <nsingh@cdac.in>
2025-03-04 23:42:11 +0530

    api gatetway for profile implemented

--- ProfileMs/src/main/java/com/Backend/ProfileMs/DTO/DoctorDTO.java
+++ ProfileMs/src/main/java/com/Backend/ProfileMs/DTO/DoctorDTO.java
@@ -15,6 +15,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DoctorDTO {
+    private Long id;
     private String name;
     private String email;
     private LocalDate dob;
